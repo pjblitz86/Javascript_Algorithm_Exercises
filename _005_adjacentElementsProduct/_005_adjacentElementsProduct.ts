752d01925cfb39b52da5a1656df80a2c1367138d
Paulius Juknis <paulius.juknis@gmail.com>
2019-02-01 10:29:19 +0200

    add solution with map and filter

--- _005_adjacentElementsProduct/_005_adjacentElementsProduct.ts
+++ _005_adjacentElementsProduct/_005_adjacentElementsProduct.ts
@@ -1,3 +1,4 @@
+// classical for loop logic
 function adjacentElementsProduct(inputArr: number[]): number {
   let largestProduct = inputArr[0] * inputArr[1];
 
@@ -13,3 +14,15 @@ function adjacentElementsProduct(inputArr: number[]): number {
 
 let inputArray = [3, 6, -2, -5, 7, 3];
 console.log(adjacentElementsProduct(inputArray));
+
+// with map and filter
+// function adjacentElementsProduct(inputArr: number[]): number {
+//   const productArray: number[] = inputArr
+//     .map((num, index) => {
+//       if (index < inputArr.length - 1) {
+//         return num * inputArr[index + 1];
+//       }
+//     })
+//     .filter(num => num !== undefined);
+//   return Math.max(...productArray);
+// }
