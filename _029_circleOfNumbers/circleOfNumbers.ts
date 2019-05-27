6fe3e36bea813e6f41173627f0c07403726b0f85
Paulius Juknis <paulius.juknis@gmail.com>
2019-05-27 14:33:23 +0300

    solution

---
+++ _029_circleOfNumbers/circleOfNumbers.ts
@@ -0,0 +1,16 @@
+function circleOfNumbers(n: number, firstNumber: number): number {
+  const numArr = [];
+
+  const halfWay = n / 2;
+  for (let i = 0; i < n; i++) {
+    numArr.push(i);
+  }
+
+  if (firstNumber < halfWay) {
+    return numArr[firstNumber + halfWay];
+  }
+
+  return numArr[firstNumber - halfWay];
+}
+
+console.log(circleOfNumbers(10, 2));
