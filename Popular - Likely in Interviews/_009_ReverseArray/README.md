6f375b27f017fc2f1632630d6b5109bde9fa606d
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-03 14:25:00 +0300

    reverse array solution

---
+++ Popular - Likely in Interviews/_009_ReverseArray/README.md
@@ -0,0 +1,1 @@
+Given an array of items, reverse the order WITHOUT in-built reverse method.
---
+++ Popular - Likely in Interviews/_009_ReverseArray/_009_ReverseArray.ts
@@ -0,0 +1,22 @@
+// traditional method with for loop, dividing arr in 2 and with temp variable.
+
+function reverseArray(inputArr: any[]): any[] {
+  for (let i = 0; i < inputArr.length / 2; i++) {
+    const temp = inputArr[i];
+    inputArr[i] = inputArr[inputArr.length - 1 - i];
+    inputArr[inputArr.length - 1 - i] = temp;
+  }
+  return inputArr;
+}
+
+// with backwards for loop and push; can also be done with unshift with asc for loop
+function _reverseArray(inputArr: any[]): any[] {
+  let newArr = [];
+  for (let i = inputArr.length - 1; i >= 0; i--) {
+    newArr.push(inputArr[i]);
+  }
+  return newArr;
+}
+
+console.log(reverseArray([1, 2, 3, 4]));
+console.log(_reverseArray([1, "4", "b", false]));
