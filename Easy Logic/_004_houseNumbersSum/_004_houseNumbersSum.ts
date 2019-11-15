54e982ed6dd93217ee77482e30010c04d9f010b8
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-15 15:49:52 +0200

    houseNumbersSum

---
+++ Easy Logic/_004_houseNumbersSum/_004_houseNumbersSum.ts
@@ -0,0 +1,12 @@
+function houseNumbersSum(inputArray: number[]): number {
+  let sum = 0;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    sum += inputArray[i];
+    if (inputArray[i] === 0) {
+      return sum;
+    }
+  }
+}
+
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
