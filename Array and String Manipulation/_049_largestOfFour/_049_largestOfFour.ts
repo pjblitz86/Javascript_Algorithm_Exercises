646b53df06a02fb654f034c05d9ad92c5acaf33b
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-25 16:23:35 +0200

    largestOfFour

---
+++ Array and String Manipulation/_049_largestOfFour/_049_largestOfFour.ts
@@ -0,0 +1,27 @@
+function largestOfFour(arr2d: number[][]): number[] {
+  const maxNumArr: number[] = [];
+
+  for (let arr of arr2d) {
+    maxNumArr.push(Math.max(...arr));
+  }
+
+  return maxNumArr;
+}
+
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+  ])
+);
+
+console.log(
+  largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1]
+  ])
+);
