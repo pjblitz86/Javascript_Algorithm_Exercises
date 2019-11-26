e0fa4f140abd47b480eb56d2cc10f2eb2b8942da
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-26 16:05:48 +0200

    lateRide

--- Array and String Manipulation/_050_lateRide/_050_lateRide.ts
+++ Array and String Manipulation/_050_lateRide/_050_lateRide.ts
@@ -1,11 +1,13 @@
 function lateRide(min: number): number {
   const hours: number = Math.floor(min / 60);
   const minutes: number = min % 60;
-  const currentTimeArr: number[] = hours.toString().concat(minutes.toString()).split('').map(char => parseInt(char));
+  const currentTimeArr: number[] = hours
+    .toString()
+    .concat(minutes.toString())
+    .split("")
+    .map(char => parseInt(char));
 
-  return currentTimeArr.reduce((total, currentValue) =>
-    total + currentValue;
-  );
+  return currentTimeArr.reduce((total, currentValue) => total + currentValue);
 }
 
 console.log(lateRide(240));
