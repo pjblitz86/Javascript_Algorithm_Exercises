8e198058bc4421237964f232616b201ffd8b249b
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-07 15:49:40 +0200

    newNumeralSystem

---
+++ Array and String Manipulation/_055_newNumeralSystem/_055_newNumeralSystem.ts
@@ -0,0 +1,18 @@
+function newNumeralSystem(numberLetter: string): string[] {
+  const numerals: string[] = [];
+  let startCharCount = 65;
+  let endCharCount = numberLetter.charCodeAt(0);
+
+  while (startCharCount <= endCharCount) {
+    const numeral = `${String.fromCharCode(
+      startCharCount
+    )} + ${String.fromCharCode(endCharCount)}`;
+    numerals.push(numeral);
+    startCharCount++;
+    endCharCount--;
+  }
+
+  return numerals;
+}
+
+console.log(newNumeralSystem("G"));
