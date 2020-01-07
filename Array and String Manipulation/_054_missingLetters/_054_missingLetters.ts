dbde3adabb05ec7f7eead352ce03429f4e68b193
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-07 11:01:42 +0200

    fix

--- Array and String Manipulation/_054_missingLetters/_054_missingLetters.ts
+++ Array and String Manipulation/_054_missingLetters/_054_missingLetters.ts
@@ -31,15 +31,13 @@ function missingLettersEasierSolution(str: string): string {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   const chars = str.split("");
 
-  if (chars[0] !== "a") {
-    return undefined;
-  }
-
   for (let i = 0; i < chars.length; i++) {
     if (chars[i] !== alphabet[i]) {
       return alphabet[i];
     }
   }
+
+  return undefined;
 }
 
 console.log(missingLetters("abce"));
